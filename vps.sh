446cb877e76fe7ad615c6dff0b8265ea04ea46ad
Jheks <36671885+Jheks@users.noreply.github.com>
2021-06-10 19:06:24 +0700

    Add files via upload

---
+++ vps.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+##code By Linuxinet##
+RED='\033[0;31m'
+GREEN='\33[0;32m'
+NC='\033[0m'
+printf "${RED}User successfully created :- Ubuntu${NC}\n"
+printf "${RED}Password successfully created :- 1234${NC}\n"
+
+
+
+printf "${GREEN}Installing RDP Be Patience....${NC}" >&2
+{
+sudo useradd -m Ubuntu
+sudo adduser Ubuntu sudo
+echo 'Ubuntu:1234' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y 
+sudo adduser Ubuntu chrome-remote-desktop
+} &> /dev/null &&
+printf "\nSetup Complete " >&2 ||
+printf "\nError Occured " >&2
+printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+read -p "Paste Here: " CRP
+su - Ubuntu -c """$CRP"""
+printf 'Check https://remotedesktop.google.com/access/ \n\n'
+if sudo apt-get upgrade &> /dev/null
+then
+    printf "\n\nUpgrade Completed " >&2
+else
+    printf "\n\nError Occured " >&2
+fi
